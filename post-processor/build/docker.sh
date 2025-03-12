ad5363f787fe974eeba8d6d7c02bd447912906cf
Aswad Tariq <86660510+aswad546@users.noreply.github.com>
2025-03-12 16:10:06 -0400

    Update docker.sh

--- post-processor/build/docker.sh
+++ post-processor/build/docker.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 set -ex
-
-echo "Hey buddy" 
-# Build the builder image
 docker build -t vv8-postprocessor-builder -f ./build/Dockerfile.builder .
+# Change the permissions of the current directory to 777
+# Change the permissions of the current working directory to 777
+chmod 777 "$(pwd)"
+ls -ld "$(pwd)"
 
-# Create named volumes (if they don't already exist)
-docker volume create rust_build_cache || true
-docker volume create go_build_cache || true
-
-# Run the builder with named volumes instead of bind mounts for caches.
+# Run the Docker command, using named volumes for build caches.
 docker run --rm -u 0 \
-  -v $(pwd):/visiblev8 \
+  -v "$(pwd)":/visiblev8:Z \
   -v rust_build_cache:/root/.cargo/registry:rw \
   -v go_build_cache:/go/pkg:rw \
   vv8-postprocessor-builder make -C /visiblev8
 
-# Build the final image
 docker build -t visiblev8/vv8-postprocessors:$(git rev-parse --short HEAD) -t vv8-postprocessors-local -f ./build/Dockerfile.vv8 .
